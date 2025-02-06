a0295d904dc320ae8d4df10eacf0dc5c1fd9613b
Chinweike99 <innocentakwolu1@gmail.com>
2025-02-06 17:22:55 +0100

    introduction to typescript

--- 0.1_basic_datatyoes/app.js
+++ 0.1_basic_datatyoes/app.js
@@ -1,2 +1,6 @@
 console.log("Hello world");
 console.log("Doing the most with every sec we have !!");
+// Variables in TypeScript.
+var n1 = 2000;
+var st = " hello";
+console.log(n1, st);
--- 0.1_basic_datatyoes/app.ts
+++ 0.1_basic_datatyoes/app.ts
@@ -1,2 +1,9 @@
 console.log("Hello world")
 console.log("Doing the most with every sec we have !!")
+
+// Variables in TypeScript.
+let n1 = 2000;
+
+const st = " hello";
+
+console.log(n1, st)
