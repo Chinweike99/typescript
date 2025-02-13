6d2e63074292ca49275c3c7f261edca4a3b6e868
Chinweike99 <innocentakwolu1@gmail.com>
2025-02-13 16:31:16 +0100

    useReducer

--- react-typescript/src/Component/Salute.tsx
+++ react-typescript/src/Component/Salute.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
-type slautation = {
-  name: string;
-}
+// import React from 'react'
+// type slautation = {
+//   name: string;
+// }
 
 
-const Salute = (props: slautation) => {
-  return (
-    <div>Hello and welcome {props.name}
+// const Salute = (props: slautation) => {
+//   return (
+//     <div>Hello and welcome {props.name}
       
+//     </div>
+
+//   )
+// }
+
+// export default Salute
+
+import React, { useReducer } from 'react';
+
+type State = { count: number };
+type Action = { type: 'increment' } | { type: 'decrement' };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
+
+const Counter: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
     </div>
-
-  )
-}
-
-export default Salute
+  );
+};
