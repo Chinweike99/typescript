34b2707568f9af146dd0f68b740999f5b2567828
Chinweike99 <innocentakwolu1@gmail.com>
2025-03-01 22:38:42 +0100

    board.tsx

--- candy_crash/src/Components/Board.tsx
+++ candy_crash/src/Components/Board.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { generateBoard } from '../utils/boardSetup'
 
 function Board() {
+
+    const [board, setBoard] = useState<string[][]>(generateBoard());
+
+
+    useEffect(() =>{
+        console.log(board);
+    }, [board])
+
   return (
     <div>Board</div>
   )
