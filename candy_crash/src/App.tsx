bc6306dfe5f32c9d7c009c9868df093369080f70
Chinweike99 <innocentakwolu1@gmail.com>
2025-03-01 22:07:49 +0100

    str: file structure

---
+++ candy_crash/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  const handleClick = (count: number) => {
+    console.log(`Count: ${count}`);
+  };
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
