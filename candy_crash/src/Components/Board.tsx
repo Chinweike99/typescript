167467d6d7b700aad56e420075d110a37ffaac74
Chinweike99 <innocentakwolu1@gmail.com>
2025-03-02 09:30:45 +0100

    chore: swap colors

--- candy_crash/src/Components/Board.tsx
+++ candy_crash/src/Components/Board.tsx
@@ -1,15 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { generateBoard } from '../utils/boardSetup'
 import Candy from './Candy';
+import { checkMatches, Swappcandies } from '../utils/gamelogic';
 
 function Board() {
 
     const [board, setBoard] = useState<string[][]>(generateBoard());
+    const [draggedCandy, setDraggedCandy] = useState<{row: number; col: number} | null>(null)
 
 
     useEffect(() =>{
         console.log(board);
+        console.log(checkMatches(board))
     }, [board])
+
+
+    const handleDragStart = (row: number, col: number) =>{
+        setDraggedCandy({row, col})
+    }
+
+    //Prevent Default behaviour
+    const handleDragOver = (event: React.DragEvent) => {
+        event.preventDefault();
+    }
+
+    const handleDrop = (row: number, col: number)=>{
+        if(draggedCandy){
+            const newBoard = Swappcandies(board, draggedCandy.row, row, draggedCandy.col, col);
+            setBoard(newBoard);
+            setDraggedCandy(null);
+        }
+    };
+
+
+
+
 
   return (
     <div className='flex flex-col items-center bg-amber-200 p-10'>
@@ -18,7 +43,12 @@ function Board() {
             return(
                 row.map((candy, colIndex) => {
                     return (
-                        <Candy key={`${rowIndex}-${colIndex}`} color={candy} row={rowIndex} col={colIndex}/>
+                        <Candy key={`${rowIndex}-${colIndex}`}
+                         color={candy} row={rowIndex} col={colIndex}
+                         onDragStart={handleDragStart}
+                         onDrop={handleDrop}
+                         onDragOver={handleDragOver}
+                         />
                     )
                 })
             )
--- candy_crash/src/Components/Candy.tsx
+++ candy_crash/src/Components/Candy.tsx
@@ -4,7 +4,7 @@ const colorClassess: Record<string, string> = {
     red: "bg-red-700",
     blue: "bg-blue-700",
     green: "bg-green-700",
-    yellow: "bg-yellow-700",
+    yellow: "bg-yellow-500",
     purple: "bg-purple-700",
     amber: "bg-amber-700",
     black: "bg-gray-700",
@@ -15,16 +15,22 @@ interface CandyProps{
     color: string;
     row: number;
     col: number;
+    onDragStart: (row: number, col: number) => void;
+    onDrop: (row: number, col: number) => void;
+    onDragOver: (event: React.DragEvent) => void;
 }
 
-const Candy: React.FC<CandyProps> = ({color, row, col}) =>{
+const Candy: React.FC<CandyProps> = ({color, row, col, onDragStart, onDrop, onDragOver}) =>{
     return (
         <div
         className={`w-10 h-10 rounded-full cursor-pointer ${colorClassess[color] || "bg-white-700"}`}
         data-row={row}
         data-col={col}
+        draggable
+        onDragStart={()=>onDragStart(row, col)}
+        onDrop={()=>onDrop(row, col)}
+        onDragOver={onDragOver}
         >
-        
         </div>
     )
 }
