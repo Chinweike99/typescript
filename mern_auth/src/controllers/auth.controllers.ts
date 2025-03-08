5a075148ec69351b63c92a9a72905a39730650b7
Chinweike99 <innocentakwolu1@gmail.com>
2025-03-08 18:45:56 +0100

    feat: register and login

--- mern_auth/src/controllers/auth.controllers.ts
+++ mern_auth/src/controllers/auth.controllers.ts
@@ -1,8 +1,9 @@
-// import { Request, Response } from "express";
-// import User from "../models/user.models";
-// import bcrypt from 'bcrypt';
-// import jwt from "jsonwebtoken";
-
+import { Request, Response } from "express";
+import User from "../models/user.models";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
 
 // export const RegisterUser = async (req: Request, res: Response): Promise<void> =>{
 //     try {
@@ -31,20 +32,19 @@
 // };
 
 
-import { Request, Response } from 'express';
-import User from '../models/user.models';
-import bcrypt from 'bcrypt';
+// import { Request, Response } from 'express';
+// import User from '../models/user.models';
+// import bcrypt from 'bcrypt';
 
 export const RegisterUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    console.log('Request Body:', req.body); // Log the request body
-
+    console.log('Request Body:', req.body);
     const { name, email, password } = req.body;
 
     // Check if the user exists
     const userExists = await User.findOne({ email });
     if (userExists) {
-      console.log('User already exists:', userExists); // Log if user exists
+      console.log('User already exists:', userExists);
       res.status(200).json({ message: 'User exists, Try Logging in' });
       return;
     }
@@ -56,11 +56,47 @@ export const RegisterUser = async (req: Request, res: Response): Promise<void> =
     // Create new User
     const user = new User({ name, email, password: hashedPw });
     await user.save();
-
-    console.log('User registered successfully:', user); // Log the registered user
+    console.log('User registered successfully:', user);
     res.status(201).json({ message: 'Registration was successful' });
   } catch (error) {
-    console.error('Error occurred:', error); // Log the error
+    console.error('Error occurred:', error);
     res.status(500).json({ message: 'An error occurred' });
   }
 };
+
+
+
+//Login a User
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if(!JWT_SECRET){
+    throw new Error ("JWT_SECRET Variable is not defined")
+}
+export const loginUser = async(req: Request, res: Response) : Promise<void> => {
+    try {
+
+        console.log("Request body", req.body);
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user){
+            res.status(400).json({message: "User does not exist"});
+            return;
+        }
+
+        const matchPw = await bcrypt.compare(password, user.password);
+        if(!matchPw){
+            res.status(400).json({message: "Passwords do not match"});
+            return;
+        }
+
+        //Generate token
+        const token = jwt.sign({userId: user._id}, JWT_SECRET, {expiresIn: "7d"});
+        res.cookie("token", token, {httpOnly: true});
+        res.json({message: "Login successful", token});
+
+    } catch (error) {
+        console.error("Error occured", error)
+        res.status(500).json({message: "Server Error"})
+    }
+}
--- mern_auth/src/routes/auth.routes.ts
+++ mern_auth/src/routes/auth.routes.ts
@@ -9,11 +9,12 @@
 // export default router;
 
 import express from 'express';
-import { RegisterUser } from '../controllers/auth.controllers';
+import { RegisterUser, loginUser } from '../controllers/auth.controllers';
 
 const router = express.Router();
 
 // Define the /register route
 router.post('/register', RegisterUser);
+router.post('/login', loginUser);
 
 export default router;
